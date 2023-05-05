4159fd83e07fc0562865d85883f79f4e03cdf620
Kerushani <kerushanisivaneswaran@gmail.com>
2023-05-05 13:26:50 -0400

    Modify loop for starting another round
    Previously, when asking the user if they wanted to start another round the program
    did not account for responses that were not 'yes' or 'no'. A while loop was added to ask for another
    response that was a valid.

--- game.js
+++ game.js
@@ -51,44 +51,56 @@ function compareSelection(playerResponse, computerResponse){
         console.log("The computer also chose " + playerResponse + " ! No one gets a point this round.");
         ties++;
     } else if (playerResponse === "ROCK" && computerResponse === "PAPER"){
-        displayLoseResult(computerResponse);
+        displayLoseResult(computerResponse, playerResponse);
 
     } else if (playerResponse === "ROCK" && computerResponse === "SCISSORS"){
-        displayWinResult(computerResponse);
+        displayWinResult(computerResponse, playerResponse);
 
     } else if (playerResponse === "PAPER" && computerResponse === "ROCK"){
-        displayWinResult(computerResponse);
+        displayWinResult(computerResponse, playerResponse);
 
     } else if (playerResponse === "PAPER" && computerResponse === "SCISSORS"){
-        displayLoseResult(computerResponse);
+        displayLoseResult(computerResponse, playerResponse);
 
     } else if (playerResponse === "SCISSORS" && computerResponse === "ROCK"){
-        displayLoseResult(computerResponse);
+        displayLoseResult(computerResponse, playerResponse);
 
     } else if (playerResponse === "SCISSORS" && computerResponse === "PAPER"){
-        displayWinResult(computerResponse);
+        displayWinResult(computerResponse, playerResponse);
     }
 
+    //displays the cumulative wins, losses and ties of the player
     console.log("Currently, you have " + wins + " wins, " + losses + " losses, and " + ties + " ties.")
 
+    //prompts player to ask if they want to play again
     let playAgain = prompt("Would you like to play again? Enter yes or no.");
     playAgain = convertToUpperCase(playAgain);
 
+    //checks if player's response is yes or no; loop will occur until valid response is given
+    while (!(playAgain == "YES" || playAgain == "NO")){
+        playAgain = prompt("Sorry, but I do not recognize that response. Would you like to play again? (Please respond with yes or no)");
+        playAgain = convertToUpperCase(playAgain);
+    }
+
     if (playAgain === "YES"){
         alert("Sounds great! Good luck for the next round!");
-        startGame();
-    } else {
-        alert("That is fine!" );
+        startGame(); //invokes function that allows the game to restart
+    } else if (playAgain === "NO"){
+        alert("That is fine!" ); //game ends at this point if player answers no
     }
 }
 
-function displayWinResult(computerResult){
-    console.log("The computer chose " + computerResult + ". Yay! You won this round!");
+//displays a message in console informing the player they won the round
+//function also increments a point to the wins variable tracker
+function displayWinResult(computerResult, playerResult){
+    console.log("The computer chose " + computerResult + " and you chose " + playerResult + ". Yay! You won this round!");
     wins++; 
 }
 
-function displayLoseResult(computerResult){
-    console.log("The computer chose " + computerResult + ". Oh no! You lost this round!");
+//displays a message in console informing the player they loss the round
+//function also increments a point to the losses variable tracker
+function displayLoseResult(computerResult, playerResult){
+    console.log("The computer chose " + computerResult + " and you chose " + playerResult + ". Oh no! You lost this round!");
     losses++;
 
 }
